f50482ef82df9eb904773eefb80031da71ba4b7b
SopheaKoy <koysophea09@gmail.com>
2023-06-06 22:04:16 +0700

    update Button addToCart

--- src/components/CartComponents.js
+++ src/components/CartComponents.js
@@ -1,7 +1,14 @@
+import { addToCart } from "@/redux/features/cart/cartSlice";
 import React from "react";
+import { useDispatch } from "react-redux";
 
 export default function CardComponents({id, name,price , qunatity , image , description}) {
 
+  const dispatch = useDispatch()
+
+  const onAddToCart = () =>{
+    dispatch(addToCart(id,name,price ,qunatity , image , description))
+  }
   return (
     <div>
       <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -65,7 +72,7 @@ export default function CardComponents({id, name,price , qunatity , image , desc
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
             </svg>
             <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-             {name ? name : "Unknow"}
+             {qunatity ? qunatity : "Unknow"}
             </span>
           </div>
           <div class="flex items-center justify-between">
@@ -73,6 +80,7 @@ export default function CardComponents({id, name,price , qunatity , image , desc
               ${price ? price : "Unknow"}
             </span>
             <a
+            onClick={()=> onAddToCart()}
               href="#"
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Add to cart
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 "use client";
 import { configureStore } from '@reduxjs/toolkit'
 import cardReducer from './features/cart/cartSlice'
-import cartSlice from './features/cart/cartSlice'
 
 // make to store 
 export const store = configureStore({
